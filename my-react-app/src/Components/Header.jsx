c3f93db6f09f07b468fc57024ad566291749939d
Krishna prasad <krishnaprasad0707cr@gmail.com>
2025-07-12 23:56:25 +0530

    Completed the header component

---
+++ my-react-app/src/Components/Header.jsx
@@ -0,0 +1,13 @@
+import HeaderImage from "../assets/card-game_4298168.png"
+import "../Components-Styles/header.css"
+
+export default function Header () {
+  return(
+    <header>
+      <div>
+        <img className="flashcards" src={HeaderImage} alt="flashcard image" />
+      <h1 className="title">Flash Card Generator</h1>
+      </div>
+    </header>
+  )
+}
