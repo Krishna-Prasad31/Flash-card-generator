bff168198f5068f80897bda7a8fe0e69ae1f355d
Krishna prasad <krishnaprasad0707cr@gmail.com>
2025-07-16 22:04:00 +0530

    Completed the styling of the topic Display

--- my-react-app/src/Components/Body.jsx
+++ my-react-app/src/Components/Body.jsx
@@ -9,6 +9,21 @@ export default function Body () {
           Submit
         </button>
       </div>
+      <div className="big">
+        <div className="list-holder">
+          <ul>
+            <li>History of Indian Civilization</li>
+            <li>Components of human brain</li>
+            <li>Important formulas of calculus</li>
+            <li>Das Kapital summary</li>
+            <li>African Wildlife(savannah)</li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <span className="final-call">Would you like to proceed with the Topics?</span>
+      <button className="proceed-button">Get Flash Cards</button>
+      </div>
     </form>
   )
 }
